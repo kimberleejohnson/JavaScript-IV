218a9cf5a040365ea23200711c9ca8253743a852
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-04 14:01:14 -0700

    Added Student class and grade function for instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -29,8 +29,34 @@ class Instructor extends Person {
     demo(subject){
         return `Today we are learning about ${subject}.`;
     }
-    // PLACEHOLDER FOR grade(student, subject)
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
 } // Instructor 
+
+// Student, extension of Person 
+class Student extends Person {
+    constructor(studentAttributes) {
+        super(studentAttributes); 
+        this.previousBackground = studentAttributes.previousBackground; 
+        this.className = studentAttributes.className; 
+        this.favSubjects= studentAttributes.favSubjects; 
+    }
+    // Methods 
+    
+    listsubjects(){
+        return `${this.favSubjects}`;
+    }
+
+    PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}.`;
+    } 
+
+    sprintChallenge(subject){
+        return `${this.name} has begun a Sprint Challenge for ${subject}.`;
+        } 
+} // Student
+
 
 // LAMBDA DATABASE 
 
@@ -46,11 +72,38 @@ const instructor1 = new Instructor ({
     catchPhrase: "Banjo time!"
 }); 
 
+const instructor2 = new Instructor ({
+    name: "David", 
+    age: 21,
+    location: "Utah", 
+    gender: "M", 
+    specialty: "front-end", 
+    favLanguage: "Python",
+    catchPhrase: "I ain't got no banjo!"
+}); 
+
+// Students 
+const student1 = new Student ({
+    name: "Kimberlee", 
+    age: 28,
+    location: "California", 
+    gender: "F", 
+    previousBackground: "Public Relations",
+    className: "Web19", 
+    favSubjects: ["HTML", "CSS", "JavaScript"] 
+}); 
+
 
 // Logging some database information to the console 
 console.log(instructor1); 
 console.log(instructor1.demo("JavaScript")); 
 console.log(instructor1.speak()); 
+console.log(student1); 
+console.log(student1.speak()); 
+console.log(student1.listsubjects()); 
+console.log(student1.sprintChallenge("JavaScript")); 
+console.log(student1.PRAssignment("JavaScript")); 
+console.log(instructor1.grade(student1, "JavaScript")); 
 
 
 // Deprecated tests 

c4afba5513267a20eb12128b516624d7f5c76968
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-04 13:23:30 -0700

    Created Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,29 @@
-// CODE here for your Lambda Classes
+//LAMBDA SCHOOL DATABASE PROJECT 
+
+// Classes for all people at Lambda School 
+
+// Person, because no matter their role everyone involved at Lambda is a person 
+class Person {
+    constructor(attributes){
+        this.name = attributes.name; 
+        this.age = attributes.age;
+        this.location = attributes.location; 
+        this.gender = attributes.gender;
+    }
+    // Methods 
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`; 
+    }
+} // Person 
+
+// Tests 
+// Testing Person class
+// const kimee = new Person ({
+//     name: "Kimee", 
+//     age: 28, 
+//     location: "San Francisco", 
+//     gender: 'F'
+// });
+
+// console.log(kimee); 
+// console.log(kimee.speak()); 
